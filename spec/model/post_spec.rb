aac69a957ea64aecc49516f1e700a73e24f542d2
pralish <pralishkayastha@gmail.com>
2019-11-08 14:32:05 +0545

    tests of comment and post model using shoulda-matcher

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Post , :type => :model do
-    it 'validates the presence of title' do
-        post = Post.new(:title => nil)
-        expect(post).to_not be_valid
-    end
+    it {should validate_presence_of :title}
+    it {should have_many :comments}
 end
